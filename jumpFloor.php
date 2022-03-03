e535e20d96b63a60517eb761445aea8ec5ed6797
wulinzhu <18515831680@163.com>
2022-03-03 19:06:54 +0800

    时间复杂度

--- jumpFloor.php
+++ jumpFloor.php
@@ -16,20 +16,31 @@
  * f(n) = f(n-1) + f(n-2)
  *
  * 斐波那契数列应用
+ * 时间复杂度 O(2n) 2的n次方，斐波那契数列 和一颗二叉树结构一样， 算某个点需要知道前两个点的值，明显呈2的指数级别增长
+ * 如果在递归的时候保存每一步得出的值，时间复杂度可以降到O(n)
  *
  */
 
-/**
- * @param $num
- * @return mixed
- */
-function jump($num) {
-    if ($num < 3) {
-        return $num;
+class Fibonacci {
+
+    public $arr = [];
+
+    /**
+     * @param $num
+     * @return mixed
+     */
+    public function jump($num) {
+        if ($num < 3) {
+            return $num;
+        }
+        if (isset($this->arr[$num]) && $this->arr[$num] > 0) {
+            return $this->arr[$num];
+        }
+        $this->arr[$num] = $this->jump($num - 1) + $this->jump($num - 2);
+        return $this->arr[$num];
     }
 
-    return jump($num - 1) + jump($num - 2);
 }
 
-$ret = jump(10);
+$ret = (new Fibonacci())->jump(20);
 print_r($ret);
